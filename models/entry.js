1f02e361d2b5ad0ac793540f29c0a3b5070ab550
kat <kat@pop-os.localdomain>
2023-11-29 14:58:51 +0000

    Delegate all entry validation to Mongoose.

--- models/entry.js
+++ models/entry.js
@@ -21,12 +21,17 @@ const entrySchema = new mongoose.Schema({
   name: {
     type: String,
     minLength: 2,
-    required: true
+    required: [true, 'Name and number are both required.'] // ? Disable frontend validation?
   },
   number: {
     type: String,
-    minLength: 11,
-    required: true
+    validate: {
+      validator: function(v) {
+        return /^0\d{9,10}$/.test(v);
+      },
+      message: props => `${props.value} isn't a valid UK phone number.`
+    },
+    required: [true, 'Name and number are both required.']
   },
 });
 entrySchema.set('toJSON', {
