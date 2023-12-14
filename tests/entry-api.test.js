5551c96e078d809b28514f123cbf67b0f093677e
kat <kat@pop-os.localdomain>
2023-12-14 13:18:46 +0000

    Create hook to reset the database and generate test data before run tests.

--- tests/entry-api.test.js
+++ tests/entry-api.test.js
@@ -2,7 +2,22 @@ import app from '../app.js';
 import supertest from 'supertest';
 import mongoose from 'mongoose';
 
+import Entry from '../models/entry.js';
+
 const api = supertest(app);
+
+const initialEntries = [
+  { name: 'Pingu', number: '07822850998' },
+  { name: 'Pinga', number: '07705362453' },
+];
+
+beforeEach(async () => {
+  await Entry.deleteMany({});
+  let entryObject = new Entry(initialEntries[0]);
+  await entryObject.save();
+  entryObject = new Entry(initialEntries[1]);
+  await entryObject.save();
+});
 
 test('Entries are returned as JSON', async () => {
   await api
@@ -14,13 +29,14 @@ test('Entries are returned as JSON', async () => {
 test('There are 2 entries', async () => {
   const response = await api.get('/api/entries');
 
-  expect(response.body).toHaveLength(2);
+  expect(response.body).toHaveLength(initialEntries.length);
 });
 
 test('The first entry is Pingu\'s', async () => {
   const response = await api.get('/api/entries');
+  const contents = response.body.map((r) => r.name);
 
-  expect(response.body[0].name).toBe('Pingu');
+  expect(contents).toContain('Pingu');
 });
 
 afterAll(async () => {
